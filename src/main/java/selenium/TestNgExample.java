df8779b1c584e58c63004dc3c45efcc327b56d11
daniel-rotariu <drotariu03@gmail.com>
2021-10-12 21:45:51 +0300

    sad

--- src/main/java/selenium/TestNgExample.java
+++ src/main/java/selenium/TestNgExample.java
@@ -1,5 +1,31 @@
 package selenium;
 
+import org.testng.annotations.Test;
+
 public class TestNgExample {
-String string;
+
+	@Test(priority=0)
+	private static void one() {
+		System.out.println("first");
+	}
+	
+	@Test(priority=1)
+	private static void second() {
+		System.out.println("second");
+	}
+	
+	@Test(priority=2)
+	private static void third() {
+		System.out.println("third");
+	}
+	
+	@Test
+	private static void fourth() {
+		System.out.println("fourth");
+	}
+	
+	@Test
+	private static void fifth() {
+		System.out.println("fifth");
+	}
 }
